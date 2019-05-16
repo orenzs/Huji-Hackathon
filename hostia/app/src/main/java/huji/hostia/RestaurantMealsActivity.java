2abd4650df331a3d12cc007f17a8060fb32e3d69
Daniel <danielkpsh@gmail.com>
2019-05-16 18:16:54 +0300

    "stuff2"

---
+++ hostia/app/src/main/java/huji/hostia/RestaurantMealsActivity.java
@@ -0,0 +1,13 @@
+package huji.hostia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantMealsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_meals);
+    }
+}
