384f92b4c15d1cf10c8e1fa967cd7366580e1f95
Daniel <danielkpsh@gmail.com>
2019-05-16 21:28:01 +0300

    "fragments working"

---
+++ hostia/app/src/main/java/huji/hostia/MainEaterActivity.java
@@ -0,0 +1,68 @@
+package huji.hostia;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class MainEaterActivity extends AppCompatActivity {
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.available_meals_tab:
+                    changeToAvailableMeals();
+                    return true;
+
+                case R.id.ordered_meals_tab:
+                    changeToOrderedMeals();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void changeToAvailableMeals() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        AvailableMealsFragment availableMealsFragment = new AvailableMealsFragment();
+
+        transaction.replace(R.id.fragment_container, availableMealsFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+        Log.d("MainEaterActivity", "available meals tab: yeah!!");
+    }
+
+    private void changeToOrderedMeals() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        OrderedMealsFragement orderedMealsFragement = new OrderedMealsFragement();
+
+        transaction.replace(R.id.fragment_container, orderedMealsFragement);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+        Log.d("MainEaterActivity", "ordered meals tab: yeah!!");
+    }
+
+        @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_available_meals);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
